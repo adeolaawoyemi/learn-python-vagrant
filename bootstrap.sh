e78aaefd1ed9d54ad4673a484755537f452b35ae
Adeola Awoyemi <adeola@creativeadea.com>
2014-03-24 10:11:13 +0000

    Update bootstrap.sh
    Add note to install supervisor - needed to start shellinabox process upon vagrant up

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,3 +16,21 @@ tar xzf shellinabox-2.14.tar.gz && \
 cd shellinabox-2.14 && \
 ./configure && make && make install && \
 cd ../ && rm -rf shellinabox-2.14
+
+# install supervisor
+# install shellinaboxd supervisor config into /etc/supervisor/conf.d/shellinaboxd.conf
+# [program:shellinaboxd]
+# command=/path/to/shellinaboxd -t -b
+# 
+# numprocs=1
+# priority=999
+# process_name=%(program_name)s_%(process_num)02d
+# user=vagrant
+# autorestart=true
+# autostart=true
+# startsecs=1
+# stopsignal=QUIT
+# stopwaitsecs=10
+# redirect_stderr=true
+# stdout_logfile=/var/log/shellinaboxd/supervisor.log
+# stdout_logfile_maxbytes=10MB
