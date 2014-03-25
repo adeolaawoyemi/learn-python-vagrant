f76b5ef4a06016424773029ebff2f510dd961cc7
Adeola Awoyemi <adeola@creativeadea.com>
2014-03-25 00:18:31 +0000

    add supervisor so we can start shellinaboxd automagically

--- Vagrantfile
+++ Vagrantfile
@@ -10,5 +10,4 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.network :forwarded_port, host: 4567, guest: 80
   config.vm.network :forwarded_port, host: 4200, guest: 4200
   config.vm.network "private_network", ip: "192.168.50.1"
-  config.vm.provision :shell, inline: "echo I am provisioning..."
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
 apt-get update
+
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo installing apache...
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 apt-get install -y apache2
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
-echo -n "PWD :"
-pwd
-
-
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo installing make...
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 apt-get install make
+
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo installing supervisor...
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+apt-get install supervisor
+
+
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo installing shellinabox...
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 cd /vagrant/installers && \
 tar xzf shellinabox-2.14.tar.gz && \
@@ -17,20 +30,36 @@ cd shellinabox-2.14 && \
 ./configure && make && make install && \
 cd ../ && rm -rf shellinabox-2.14
 
-# install supervisor
-# install shellinaboxd supervisor config into /etc/supervisor/conf.d/shellinaboxd.conf
-# [program:shellinaboxd]
-# command=/path/to/shellinaboxd -t -b
-# 
-# numprocs=1
-# priority=999
-# process_name=%(program_name)s_%(process_num)02d
-# user=vagrant
-# autorestart=true
-# autostart=true
-# startsecs=1
-# stopsignal=QUIT
-# stopwaitsecs=10
-# redirect_stderr=true
-# stdout_logfile=/var/log/shellinaboxd/supervisor.log
-# stdout_logfile_maxbytes=10MB
+
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo installing shellinaboxd supervisor config...
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+CONF_DIR=/etc/supervisor/conf.d
+
+if [ ! -d "$CONF_DIR" ]; then
+	mkdir -p $CONF_DIR
+fi
+
+SUPERVISOR_CONF=/etc/supervisor/conf.d/shellinaboxd.conf
+SHELLINABOX=`which shellinaboxd`
+
+(
+cat <<EOF
+[program:shellinaboxd]
+command=${SHELLINABOX} -t -b
+
+numprocs=1
+priority=999
+process_name=%(program_name)s_%(process_num)02d
+user=vagrant
+autorestart=true
+autostart=true
+startsecs=1
+stopsignal=QUIT
+stopwaitsecs=10
+redirect_stderr=true
+stdout_logfile=/var/log/shellinaboxd/supervisor.log
+stdout_logfile_maxbytes=10MB
+EOF
+) > $SUPERVISOR_CONF
